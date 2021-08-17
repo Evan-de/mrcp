72d27d81c3f54e962afc2c11e480ee574322c6a6
Evan <evandde@gmail.com>
2021-08-17 16:09:03 +0900

    Add Results

--- mrcp_g4/src/Run.cc
+++ mrcp_g4/src/Run.cc
@@ -29,8 +29,33 @@ void Run::RecordEvent(const G4Event* anEvent)
     std::map<G4String, G4double> protQMap;
     protQMap["01. WholeBodyDose"] = mainPhantomProtQ->GetWholebodyDose(doseMap);
     protQMap["02. EffectiveDose"] = mainPhantomProtQ->GetEffectiveDose(doseMap);
-    protQMap["03. Lungs"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Lungs, doseMap);
-    protQMap["04. Thyroid"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Thyroid, doseMap);
+    protQMap["11. RedBoneMarrow"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::RedBoneMarrow, doseMap);
+    protQMap["12. Colon"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Colon, doseMap);
+    protQMap["13. Lungs"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Lungs, doseMap);
+    protQMap["14. Stomach"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Stomach, doseMap);
+    protQMap["15. Breast"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Breast, doseMap);
+    protQMap["16. Gonads"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Gonads, doseMap);
+    protQMap["17. Bladder"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Bladder, doseMap);
+    protQMap["18. Liver"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Liver, doseMap);
+    protQMap["19. Oesophagus"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Oesophagus, doseMap);
+    protQMap["20. Thyroid"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Thyroid, doseMap);
+    protQMap["21. BoneSurface"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::BoneSurface, doseMap);
+    protQMap["22. Brain"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Brain, doseMap);
+    protQMap["23. SalivaryGlands"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::SalivaryGlands, doseMap);
+    protQMap["24. Skin"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Skin, doseMap);
+    protQMap["25. Adrenals"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Adrenals, doseMap);
+    protQMap["26. Extrathoracic"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Extrathoracic, doseMap);
+    protQMap["27. GallBladder"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::GallBladder, doseMap);
+    protQMap["28. Heart"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Heart, doseMap);
+    protQMap["29. Kidneys"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Kidneys, doseMap);
+    protQMap["30. LymphaticNodes"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::LymphaticNodes, doseMap);
+    protQMap["31. Muscle"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Muscle, doseMap);
+    protQMap["32. OralMucosa"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::OralMucosa, doseMap);
+    protQMap["33. Pancreas"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Pancreas, doseMap);
+    protQMap["34. ProstateUterus"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::ProstateUterus, doseMap);
+    protQMap["35. SmallIntestine"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::SmallIntestine, doseMap);
+    protQMap["36. Spleen"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Spleen, doseMap);
+    protQMap["37. Thymus"] = mainPhantomProtQ->GetOrganDose(MRCPProtQCalculator::Organ::Thymus, doseMap);
 
     // Store the quantities and their squared values
     for(const auto& protQ: protQMap)
